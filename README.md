c669012b6662db20405d788b460180a42f176f06
Marcel Suter <marcel.suter@nowhow.ch>
2025-01-09 08:58:56 +0100

    Updated wordings

--- README.md
+++ README.md
@@ -2,16 +2,15 @@
 
 A Firefox extension that caches all view-only pages from nowhow's Foswiki instance, to speed up navigation.
 
-- Cached pages are indicated by a watermark
+- Cached pages are indicated by a (fading) watermark
 - Cached pages expire after 4 hours
 - Pages for manipulating content, like editing and attaching files are not cached.
+- Pages for logging into the Azure account are not cached.
 - The default reload actions by the browser (F5, CTRL-F5 and the cache setting in the DEV tools) are not affected
 
-Later improvements
+Possible later improvements
 
 - Paging can be toggled via a button besides the address bar or via the CTRL-F8 key combination
-- Caching is disabled temporarily after the redirect upon an edit of a wiki topic
-
 
 ## Technical details
 
--- speedify.js
+++ speedify.js
@@ -1,10 +1,16 @@
 
-// From https://stackoverflow.com/a/51817474/79485
-var isCached = performance.getEntriesByType("navigation")[0].transferSize === 0;
-if (isCached) {
-    console.debug('Speedify: page served form cache');
-    document.body.classList.add("is-cached");
+/** Indicate whether the current page was retrieved from the cache
+ */
+function indicateCacheUsage(){
+    // Taken from https://stackoverflow.com/a/51817474/79485
+    const isCached = performance.getEntriesByType("navigation")[0].transferSize === 0;
+    if (isCached) {
+        console.debug('Speedify: page served form cache');
+        document.body.classList.add("is-cached");
+    }
+    else {
+        console.debug('Speedify: page served form server');
+    }
 }
-else {
-    console.debug('Speedify: page served form server');
-}
+
+indicateCacheUsage();
